d01c14ae3d63e0ce3009869eb25ccf636e87ade7
John Heiller Martinez <mjohnheiller@ymail.com>
2024-05-03 13:09:24 -0500

    feat: HU - Realizar pedidos para usuarios normales

---
+++ Tecnofactory.Alimentos/DAL/Entity/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tecnofactory.Alimentos.DAL.Entity
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid CatalogueId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ Tecnofactory.Alimentos/DAL/Repository/OrderRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Tecnofactory.Alimentos.DAL.Entity;
+using Tecnofactory.Alimentos.DAL.Repository.Interface;
+
+namespace Tecnofactory.Alimentos.DAL.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public OrderRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Guid Create(Order order)
+        {
+            _dbContext.Orders.Add(order);
+            _dbContext.SaveChanges();
+            return order.Id;
+        }
+
+        public List<Order> Get()
+        {
+            return _dbContext.Orders
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public List<Order> Get(Expression<Func<Order, bool>> predicate)
+        {
+            return _dbContext.Orders
+                .Where(predicate)
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public void Update(Order order)
+        {
+            _dbContext.Entry(order).State = EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(Order order)
+        {
+            _dbContext.Orders.Remove(order);
+            _dbContext.SaveChanges();
+        }
+    }
+}
